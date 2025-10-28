623e5d801cb4bc5239e14c913b16836158a032e9
ZainAlfai <zainalfaie3@gmail.com>
2025-10-28 16:33:17 +0300

    PRIZM Node: Updated Subscribers & Publishers

--- src/vlns/src/PRIZM_node/PRIZM_node.ino
+++ src/vlns/src/PRIZM_node/PRIZM_node.ino
@@ -1,14 +1,19 @@
 #include <PRIZM.h>          
 #include <ros.h>
 #include <std_msgs/String.h>
+
+
+#include <std_msgs/Bool.h>
 #include <std_msgs/Float32.h>
 
-
-
+const int isCar =2;
+int t=10000, last_time=0;
 
 PRIZM prizm; 
 ros::NodeHandle  nh;
 std_msgs::Float32 enc;
+std_msgs::Bool str_msg;
+
 
 String mode;
 int tmp=0;
@@ -18,6 +23,7 @@ mode = msg.data;
 }
 
 ros::Publisher encoder_pub("/energy_storage/encoder", &enc);
+ros::Publisher chatter("/car_passed", &str_msg);
 ros::Subscriber<std_msgs::String> sub("mode", messageCb );
 
 
@@ -25,16 +31,20 @@ ros::Subscriber<std_msgs::String> sub("mode", messageCb );
 
 
 void setup() {
+
   Serial.begin(57600);
   prizm.PrizmBegin();         
   nh.initNode();
   nh.subscribe(sub);
   nh.advertise(encoder_pub);
+  nh.advertise(chatter);
   delay(300);
   prizm.setMotorPower(2,0);
   delay(300);
   enc.data = 0.0;
   encoder_pub.publish( &enc );
+  pinMode(isCar, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(isCar), onLow, FALLING);
     
 }
 
@@ -57,7 +67,7 @@ void control(){
     delay(600);
     prizm.setRedLED(LOW);
     delay(600);
-    prizm.setServoPosition(1,10);  
+    prizm.setServoPosition(1,30);  
     }
     else if (mode == "fast"){
       delay(200);
@@ -70,7 +80,7 @@ void control(){
     prizm.setMotorPower(2,0);
     prizm.setRedLED(LOW);
     prizm.setGreenLED(HIGH);  
-    prizm.setServoPosition(1,10);  
+    prizm.setServoPosition(1,30);  
   
     }
     tmp = prizm.readEncoderCount(2);
@@ -84,5 +94,23 @@ void control(){
 
 void loop() {
 control();
+
+if(millis()-t>1500 && last_time==1){
+  str_msg.data=0;
+  last_time=0;
+  chatter.publish(&str_msg);
+}
+delay(100);
 nh.spinOnce();
+}
+
+void onLow()
+{
+  if(millis()-t>1500 && last_time==0){
+      t=millis();
+      str_msg.data=1;
+      last_time=1;
+      chatter.publish(&str_msg);
+  }
+  
 }
